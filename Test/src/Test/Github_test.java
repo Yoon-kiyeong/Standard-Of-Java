7d6346f0e3a59c331be3fdcfc9fdea655b86df76
Yoon-kiyeong <kyyoon2105@naver.com>
2022-11-15 23:32:22 +0900

    Github Test
    github commit Test

---
+++ Test/src/Test/Github_test.java
@@ -0,0 +1,10 @@
+package Test;
+
+public class Github_test {
+
+	public static void main(String[] args) {
+		System.out.println("Hello World!");
+
+	}
+
+}
--- Test/src/test.java
+++
@@ -1,9 +0,0 @@
-
-public abstract class test {
-
-	public static void main(String[] args) {
-		System.out.println("test");
-
-	}
-
-}
