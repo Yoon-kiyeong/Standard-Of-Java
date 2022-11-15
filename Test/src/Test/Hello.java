5516d0b1ea4d575b4c7052f286760716aac812a0
Yoon-kiyeong <kyyoon2105@naver.com>
2022-11-15 23:40:35 +0900

    Chapter 1
    Comment

---
+++ Test/src/Test/Hello.java
@@ -0,0 +1,18 @@
+package Test;
+/*
+Date : 2022-11-15
+Source : Hello.Java
+Author : 윤기영
+Email : kyyoon2105@naver.com
+*/
+
+public class Hello {
+
+	public static void main(String[] args) { // 프로그램의 시작
+		System.out.println("Hello World!");	//Hello, World!를 출력
+		System.out.println("Hello, /* 이것은 주석 아님*/ world");
+		System.out.println("Hello, world. // 이것도 주석 아님");
+		
+	}
+
+}
