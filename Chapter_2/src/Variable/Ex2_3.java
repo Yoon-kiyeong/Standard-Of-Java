0dcb9c40bb51c6ea41db9bf140b815e1f9439a87
Yoon-kiyeong <kyyoon2105@naver.com>
2022-11-17 23:31:47 +0900

    Chapter 2
    Chapter 2 변수

--- Chapter_2/src/Variable/Ex2_3.java
+++ Chapter_2/src/Variable/Ex2_3.java
@@ -14,5 +14,15 @@ public class Ex2_3 {
 		System.out.println(5-3);	//화면에 5-3의 결과인 2가 출력된다.
 		System.out.println(5*3);	//화면에 5*3의 결과인 15가 출력된다.
 		System.out.println(5/3);	//화면에 5/3의 결과인 1이 출력된다.
+		
+		//변수를 이용하면 각 변수에 다른 값만 저장하고 나머지 부분은 바꾸지 않아도 된다.
+		int x = 5;	//변수에 다른 값을 저장하기만 하면 된다.
+		int y = 3;	//변수에 다른 값을 저장하기만 하면 된다.
+		System.out.println(x + y);
+		System.out.println(x - y);
+		System.out.println(x * y);
+		System.out.println(x / y);
+		
+		//변수를 사용하지 않았을 때 보다 한결 편리하다. 이것만으로도 변수가 왜 필요한지 충분히 이해할 수 있을 것이다.
 	}
 }
