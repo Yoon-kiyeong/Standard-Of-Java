fd896c69c42461bb92648f22b5c3bc81c745e894
Yoon-kiyeong <kyyoon2105@naver.com>
2022-11-25 00:17:59 +0900

    Chapter 3
    Chapter 3 연산자

---
+++ Chapter_3/src/Operator/Ex3_13.java
@@ -0,0 +1,9 @@
+package Operator;
+
+public class Ex3_13 {
+	public static void main(String[] args) {
+		System.out.println(-10%8);
+		System.out.println(10%-8);
+		System.out.println(-10%-8);
+	}
+}
